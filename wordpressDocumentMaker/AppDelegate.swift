062017caf21b375b01f80b8faa90da5742b73eec
moxou <moxou@mac.com>
2016-04-09 08:50:35 +0200

    Initial Commit

---
+++ wordpressDocumentMaker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  wordpressDocumentMaker
+//
+//  Created by Guillaume Escrivant on 09/04/2016.
+//  Copyright © 2016 ___moxsoft___. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ wordpressDocumentMaker/Document.swift
@@ -0,0 +1,28 @@
+//
+//  Document.swift
+//  wordpressDocumentMaker
+//
+//  Created by Guillaume Escrivant on 09/04/2016.
+//  Copyright © 2016 ___moxsoft___. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class func autosavesInPlace() -> Bool {
+        return true
+    }
+
+    override var windowNibName: String? {
+        // Returns the nib file name of the document
+        // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this property and override -makeWindowControllers instead.
+        return "Document"
+    }
+
+}
